ec7a37a59659743cd65ac8dae4482f5d217a6688
RKim220 <ryan.kim@gmail.com>
2014-11-18 02:57:20 -0800

    added user login and session login via rails only.  Can create new user  via modal in nav bar, but modal cancel button doesn't work yet.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@ class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
 
-  def index
+  helper_method :current_user
+
+  # before_action :authorize
+
+  def current_user
+    @current_user ||= User.where(id: session[:user_id]).first
   end
+
+  # def authorize
+  #   redirect_to new_sessions_path if current_user.nil?
+  # end
+
+
 end
