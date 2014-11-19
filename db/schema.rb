5a653deb4bb27a4ee4b185025b56d9f6b356b970
Sophana Sok <ssokprivate@hotmail.com>
2014-11-18 18:42:21 -0800

    run rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119015715) do
+ActiveRecord::Schema.define(version: 20141119021555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: true do |t|
+    t.string   "address_id"
+    t.string   "street_address"
+    t.string   "city"
+    t.string   "state_id"
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "ratings", force: true do |t|
     t.integer  "rating"
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -5,13 +5,19 @@ describe Address do
 
   xit "has a street address" do
 
+    it { should validate_presence_of(:street_address) }
+
   end
 
   xit "has a city" do
 
+    it { should validate_presence_of(:city) }
+
   end
 
   xit "has a state" do
+
+    it { should validate_presence_of(:state) }
 
   end
 
