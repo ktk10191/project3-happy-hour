f4fbb66afb4cc6735e208fe0fd15c884c8237122
RKim220 <ryan.kim@gmail.com>
2014-11-19 19:28:04 -0800

    cleaned up some user and business tests, all pass for now

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -6,26 +6,20 @@ describe Business, :type => :model do
    # validate business has a phone number
   it { should validate_presence_of(:phone_number) }
 
+   # validates unique phone number
+  it { should validate_uniqueness_of(:phone_number) }
+
   # validate phone number is 10 digits
-  it { should ensure_length_of(:phone_number).is(10) }
+  it { should ensure_length_of(:phone_number).is_equal_to(10) }
 
-  # validate phone number is an integer
-  it { should validate_numericality_of(:phone_number).only_integer }
+  # validate phone number format
+  it { should allow_value('1234567890').for(:phone_number) }
 
-  # validate business has ratings
-  it { should have_many(:ratings) }
+#   # validate business has ratings
+#   it { should have_many(:ratings) }
 
-  # validate business has an address
-  it { should has_one(:address) }
+#   # validate business has an address
+#   it { should has_one(:address) }
 
 
 end
-
-
-  it { should validate_presence_of(:name) }
-
-  it { should ensure_length_of(:name).is_at_least(1) }
-
-  it { should ensure_length_of(:name).is_at_most(50) }  
-
-  it { should belong_to(:list) }
