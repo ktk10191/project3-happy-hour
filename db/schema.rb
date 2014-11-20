e9835e2c4662c6c1a655c64de233693ec025974e
ktk10191 <kyleknott11111@gmail.com>
2014-11-19 16:23:22 -0800

    merged with test stuff successfully with no errors

--- db/schema.rb
+++ db/schema.rb
@@ -16,16 +16,6 @@ ActiveRecord::Schema.define(version: 20141119031111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "addresses", force: true do |t|
-    t.string   "address_id"
-    t.string   "street_address"
-    t.string   "city"
-    t.string   "state_id"
-    t.string   "state"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "ratings", force: true do |t|
     t.integer  "rating"
     t.datetime "created_at"
