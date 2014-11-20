0bd5a66d22d14b0d5d04d99b5a71ab4b5ead3080
Cosimo Barbato <cbarb001@gmail.com>
2014-11-20 15:14:01 -0800

    photo upload

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141119215813) do
+ActiveRecord::Schema.define(version: 20141120223038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20141119215813) do
     t.datetime "updated_at"
   end
 
+  create_table "images", force: true do |t|
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.string   "image_file_size"
+    t.datetime "image_updated_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "ratings", force: true do |t|
     t.integer  "rating"
     t.datetime "created_at"
@@ -39,8 +48,6 @@ ActiveRecord::Schema.define(version: 20141119215813) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username"
-    t.string   "name"
   end
 
 end
