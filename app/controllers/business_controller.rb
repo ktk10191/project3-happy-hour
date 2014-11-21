2fb2e3ba85c62673ebd012260e3c8950e35b8c79
ktk10191 <kyleknott11111@gmail.com>
2014-11-20 20:37:30 -0800

    made changes to business controller and routes. home html was also deleted

--- app/controllers/business_controller.rb
+++ app/controllers/business_controller.rb
@@ -1,23 +1,29 @@
-class BusinessesController < ApplicationController
+class BusinessController < ApplicationController
   
-  def new
-    @business = Business.new
-  end
-
-  def create
-    @business = Business.new(business_params)
-
-    if @business.save
-      # Add alert that says the business to be updated
+    def index
       redirect_to root_url
-    else
-      # Add Alert that says the business fields were not put in correctly. This will more than likely have the form tell them that it is invalid
-      redirect_to(:back)
     end
-    private
-      def business_params
-        # Need to add more params later for businesses
-        params.require(:business).permit(:business_name, :setting)
-      end
- end
 
+    def new
+      @business = Business.new
+    end
+
+    def create
+      @business = Business.new(business_params)
+
+      if @business.save
+        # Add alert that says the business to be updated
+        redirect_to root_url
+      else
+        # Add Alert that says the business fields were not put in correctly. This will more than likely have the form tell them that it is invalid
+        redirect_to(:back)
+      end
+    end
+
+      private
+        def business_params
+          # Need to add more params later for businesses
+          params.require(:business).permit(:business_name, :setting)
+        end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,9 @@ Project3HappyHour::Application.routes.draw do
   post 'sessions' => 'sessions#create'
   delete 'sessions' => 'sessions#destroy'
 
-  get 'businesses/' => 'businesses#index'
-  get 'businesses/new' => 'businesses#new', as: :new_business
-  get 'businesses/:id' => 'businesses#show', as: :business
-  post 'businesses' => 'businesses#create', as: :create_business
-  get 'businesses/:id/edit' => 'businesses#edit', as: :edit_business
-  patch 'businesses/:id' => 'businesses#update'  
+  post 'businesses' => 'business#create', as: :create_business
+  get 'business/:id/edit' => 'business#edit', as: :edit_business
+  patch 'business/:id' => 'business#update'  
 
   # get '*path' => 'application#index'
 end
