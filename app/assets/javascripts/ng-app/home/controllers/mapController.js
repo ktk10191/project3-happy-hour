cdfc74e388cc770632b604c4f02942504e5ba377
RKim220 <ryan.kim@gmail.com>
2014-11-28 16:09:48 -0800

    got lat and long to store into the business array

--- app/assets/javascripts/ng-app/home/controllers/mapController.js
+++ app/assets/javascripts/ng-app/home/controllers/mapController.js
@@ -9,51 +9,22 @@ angular.module('happyHrApp')
         // Specifically pushing the json data into the array
         $scope.businesses.push(data.data);
         // Making sure everything works by logging the newly populated array
-        console.log($scope.businesses);
+        // console.log($scope.businesses);
 
         for(var i = 0; i < $scope.businesses[0].length; i++) {
-          // console.log($scope.businesses[i].street_name);
-          // console.log($scope.businesses[i].city);
-          // console.log($scope.businesses[i].zipcode);
           $scope.fullAddress = $scope.businesses[0][i].street_no + ' ' + $scope.businesses[0][i].street_name + ', ' + $scope.businesses[0][i].city + ', ' + $scope.businesses[0][i].zipcode; 
-          console.log($scope.fullAddress);
-          $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' +$scope.fullAddress+ '&sensor=false')
-            .then(function(data) {
-              var p = data.data.results[0].geometry.location;
-              latlng = {lat: p.lat, lng: p.lng};
-              $scope.businesses.push(latlng);
-              console.log(latlng);
-            });
-            console.log(i);
-            console.log($scope.businesses);
-            // $scope.businesses.latlng = latlng;
-            // console.log($scope.businesses.latlng);
+          (function(counter) {
+            $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' +$scope.fullAddress)
+              .then(function(data) {
+                var p = data.data.results[0].geometry.location;
+                var lat = p.lat;
+                var lng = p.lng;
+                $scope.businesses[0][counter].lat = lat;
+                $scope.businesses[0][counter].lng = lng;
+                console.log($scope.businesses[0][counter]);
+              })
+            }(i));
         }
       })
     }
   });
-
-  //   $scope.addAddresses = function() {
-  //     for(var i = 0; i < $scope.businesses.length; i++) {
-  //       $scope.fullAddress = $scope.businesses[i].street_no + ' ' + $scope.businesses[i].street_name + ', ' + $scope.businesses[i].city + ', ' + $scope.businesses[i].zipcode; 
-  //       console.log($scope.fullAddress);
-  //       $http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' +$scope.fullAddress[i]+ '&key=AIzaSyDdZLvJpNUODunYI4Imfm4elrUI7y66dqc')
-  //         .then(function() {
-  //           $scope.businesses[i].geometry.location.lat = lat;
-  //           $scope.businesses[i].geometry.location.lng = lng;
-  //           console.log(lat);
-  //         })
-  //     }
-  //   }
-  // })
-
-
-
-
-
-  //   .controller('mapController', function($scope, api) {
-  //   $scope.mapMarkers = api.getBusinesses().then(function (data){
-  //        console.log(data)
-  //        $scope.businesses = data.data
-  //      });
-  // })
