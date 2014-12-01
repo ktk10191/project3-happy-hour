ef27e7c6e0d4fdc2bf01f4917e74bd0a1a2e2c7e
Sophana Sok <ssokprivate@hotmail.com>
2014-12-01 15:04:14 -0800

    add lorem ipsum text to bios and made comments to bioController.js

--- app/assets/javascripts/ng-app/home/controllers/bioController.js
+++ app/assets/javascripts/ng-app/home/controllers/bioController.js
@@ -1,5 +1,6 @@
 angular.module('happyHrApp')
 
+// link to div id for individual dev team pages
 .controller('bioController', ['$scope', '$location', '$anchorScroll', function($scope, $location, $anchorScroll){
   $scope.scrollTo = function(id) {
       $location.hash(id);
