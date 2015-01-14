124fc28ef1ddbf5e997a9502e6cae428b712de3b
RKim220 <ryan.kim@gmail.com>
2015-01-14 12:57:31 -0800

    update footer

--- app/assets/javascripts/ng-app/home/controllers/businessFormController.js
+++ app/assets/javascripts/ng-app/home/controllers/businessFormController.js
@@ -2,7 +2,6 @@ angular.module('happyHrApp')
 
 .controller('businessFormController', function($scope, api) {
   $scope.addToBusinessApi = function(data) {
-    console.log("add to business");
     console.log(data)
     api.createBusiness($scope.bName, $scope.addr, $scope.street, $scope.city, $scope.zipcode, $scope.state, $scope.phone, $scope.website, $scope.rating, $scope.happyHourTime);
 
